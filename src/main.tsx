634ac263708b9ad5afbf0bcd2ecbca3604c18912
adhamsabryco-maker <adhamsabry.co@gmail.com>
2026-03-04 06:31:33 +0200

    Add files via upload

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,10 @@
-import {StrictMode} from 'react';
-import {createRoot} from 'react-dom/client';
-import { AvatarProvider } from './contexts/AvatarContext';
-import App from './App.tsx';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 import './index.css';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <AvatarProvider>
-      <App />
-    </AvatarProvider>
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
 );
