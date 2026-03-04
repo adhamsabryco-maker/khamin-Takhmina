9db2131382b66c35a53ec501319738e683b3e284
adhamsabryco-maker <adhamsabry.co@gmail.com>
2026-03-04 08:20:09 +0200

    Add files via upload

---
+++ src/constants.ts
@@ -0,0 +1,23 @@
+export const STATIC_ASSETS = {
+  avatars: {
+    1: ['avatar-free-01.png', 'avatar-free-02.png', 'avatar-free-03.png', 'avatar-free-04.png'],
+    10: 'avatar-lvl-10.png',
+    20: 'avatar-lvl-20.png',
+    30: 'avatar-lvl-30.png',
+    40: 'avatar-lvl-40.png',
+  },
+  frames: {
+    10: 'frame-lvl-10.png',
+    20: 'frame-lvl-20.png',
+    30: 'frame-lvl-30.png',
+    40: 'frame-lvl-40.png',
+    50: 'frame-lvl-50.png',
+  },
+  stars: {
+    10: 'star-lvl-10.png',
+    20: 'star-lvl-20.png',
+    30: 'star-lvl-30.png',
+    40: 'star-lvl-40.png',
+    50: 'star-lvl-50.png',
+  }
+};
