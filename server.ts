7191346abf2343b660059bce566d6d6c8211fec0
adhamsabryco-maker <adhamsabry.co@gmail.com>
2026-03-04 05:49:15 +0200

    Add files via upload

--- server.ts
+++ server.ts
@@ -12,7 +12,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const storage = multer.diskStorage({
-  destination: 'public/uploads/',
+  destination: path.join(__dirname, 'public/uploads/'),
   filename: (req, file, cb) => {
     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
     cb(null, 'image-' + uniqueSuffix + path.extname(file.originalname));
