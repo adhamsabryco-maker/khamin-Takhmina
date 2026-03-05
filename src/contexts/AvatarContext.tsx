d1406c5f867afca6b0704e1aa9931b4aa6d7d648
adhamsabryco-maker <adhamsabry.co@gmail.com>
2026-03-05 12:05:23 +0200

    Add files via upload

--- src/contexts/AvatarContext.tsx
+++ src/contexts/AvatarContext.tsx
@@ -3,7 +3,7 @@ import React, { createContext, useContext, useState, useEffect } from 'react';
 const AvatarContext = createContext<any>(null);
 
 export const AvatarProvider = ({ children }: { children: React.ReactNode }) => {
-  const [customConfig, setCustomConfig] = useState<any>({ avatars: {}, frames: {}, stars: {} });
+  const [customConfig, setCustomConfig] = useState<any>({ avatars: {}, frames: {}, stars: {}, aiBotEnabled: false });
 
   const refreshConfig = () => {
     fetch('/api/config').then(res => res.json()).then(setCustomConfig);
